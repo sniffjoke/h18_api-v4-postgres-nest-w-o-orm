3844ead7517749b56bc947f5f3c5b406c1efeaec
sniffjoke <sniffjoke@gmail.com>
2024-10-22 23:45:41 +0200

    first commit

---
+++ src/features/testing/application/testing.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+
+@Injectable()
+export class TestingService {
+  constructor(
+    @InjectDataSource() private readonly dataSource: DataSource,
+  ) {
+  }
+
+  async deleteAll() {
+      await this.dataSource.query('TRUNCATE TABLE users')
+      await this.dataSource.query('TRUNCATE TABLE devices')
+      await this.dataSource.query('TRUNCATE TABLE tokens')
+  }
+
+}
